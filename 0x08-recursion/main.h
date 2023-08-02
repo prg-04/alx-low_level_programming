7e637b681615e6bb7cbb08e5aa2515dcc6cc104b
prg-04 <karanjaevans95@gmail.com>
2023-08-02 09:17:03 -0700

    Fix bug

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,8 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
